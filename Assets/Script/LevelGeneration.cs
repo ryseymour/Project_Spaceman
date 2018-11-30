70931abd4caddc26adbdd6384fa87da831b75681
ryseymour <ry.seymour@gmail.com>
2018-11-30 17:35:06 -0500

    Inital Upload Level Design

---
+++ Assets/Script/LevelGeneration.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGeneration : MonoBehaviour {
+
+    public Transform[] startingPositions;
+    public GameObject[] rooms;
+
+    private int direction;
+    public float moveAmount;
+
+    private float timeBtwRoom;
+    public float startTimeBtwRoom = 0.25f;
+
+    public float minX;
+    public float maxX;
+    public float minY;
+   public bool stopGeneration;
+
+    public LayerMask room;
+
+    private int downCounter;
+
+    private void Start()
+    {
+        int randStartingPos = Random.Range(0, startingPositions.Length);
+        transform.position = startingPositions[randStartingPos].position;
+        Instantiate(rooms[0], transform.position, Quaternion.identity);
+
+        direction = Random.Range(1, 6);
+    }
+
+    private void Update()
+    {
+        if (timeBtwRoom <= 0 && stopGeneration == false)
+        {
+
+            Move();
+            timeBtwRoom = startTimeBtwRoom;
+        }
+        else
+        {
+            timeBtwRoom -= Time.deltaTime;
+        }
+
+    }
+
+    private void Move()
+    {
+
+        if (direction == 1 || direction == 2) //Move Right
+        {
+            if(transform.position.x < maxX)
+            {
+                downCounter = 0;
+                Vector2 newPos = new Vector2(transform.position.x + moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(1, 6);
+                if (direction == 3)
+                {
+                    direction = 2;
+                } else if (direction == 4){
+                    direction = 5;
+                }
+            }
+            else
+            {
+                direction = 5;
+            }
+
+        }
+
+        else if (direction == 3 || direction == 4)//Move Left
+        {
+        
+
+            if(transform.position.x >minX)
+            {
+                downCounter = 0;
+                Vector2 newPos = new Vector2(transform.position.x - moveAmount, transform.position.y);
+                transform.position = newPos;
+
+                int rand = Random.Range(0, rooms.Length);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+
+                direction = Random.Range(3, 6);
+            }
+            else
+            {
+                direction = 5;
+            }
+        }
+
+        else if (direction == 5) //Move Down
+        {
+            downCounter++;
+            if(transform.position.y > minY)
+            {
+                Collider2D roomDetection = Physics2D.OverlapCircle(transform.position, 1, room);
+                if(roomDetection.GetComponent<RoomType>().type != 1 && roomDetection.GetComponent<RoomType>().type != 3)
+                {
+                    if (downCounter >= 2)
+                    {
+                        roomDetection.GetComponent<RoomType>().RoomDestruction();
+                        Instantiate(rooms[3], transform.position, Quaternion.identity);
+                    }
+                    else{
+                        roomDetection.GetComponent<RoomType>().RoomDestruction();
+
+                        int randBottomRoom = Random.Range(1, 4);
+                        if (randBottomRoom == 2)
+                        {
+                            randBottomRoom = 1;
+                        }
+                        Instantiate(rooms[randBottomRoom], transform.position, Quaternion.identity);
+                    }
+
+                }
+                Vector2 newPos = new Vector2(transform.position.x, transform.position.y - moveAmount);
+                transform.position = newPos;
+
+                int rand = Random.Range(2, 4);
+                Instantiate(rooms[rand], transform.position, Quaternion.identity);
+                direction = Random.Range(1, 6);
+                
+
+            }
+            else
+            {
+                stopGeneration = true;
+
+            }
+
+        }
+
+        
+       
+
+    }
+}
---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public CharacterController2D controller;
+    public float runSpeed = 40f;
+    float horizontalMove = 0f;
+    bool jump = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update() {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+
+    }
+
+    void FixedUpdate ()
+    {
+        //Move Character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+    }
+}
